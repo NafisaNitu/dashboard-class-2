4bbaa1862d1df68192e5e6d776c2683b1fee3d84
NafisaNitu <49776498+NafisaNitu@users.noreply.github.com>
2025-07-02 23:48:42 +0600

    Update HeroSection.jsx

--- src/frontend/home/HeroSection.jsx
+++ src/frontend/home/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <section className='bg-[url(assets/hero.png)] sm:min-h-[700px] min-h-[500px] bg-center'>
+    <section className="bg-[url('/assets/hero.png')] sm:min-h-[700px] min-h-[500px] bg-center">
         <div className='max-w-4xl mx-auto text-white text-center flex flex-col sm:gap-10 gap-6 px-6 sm:pt-20 pt-8'>
             <h2 className='xl:text-5xl text-2xl font-bold capitalize text-yellow-500'>The Book Lover's Dreamland Awaits!</h2>
             <p className='xl:text-lg'>Welcome to the ultimate book lover's paradise! Join our community and contribute to the ever-evolving library of stories, where every book has a chance to inspire someone new.</p>
